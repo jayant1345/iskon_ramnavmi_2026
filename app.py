4c2db83a11fa5d10fb521a337f487303ff0471fb
jayant1345 <jayswal1bsnl@gmail.com>
2026-03-28 14:59:10 +0530

    set railway URL and dynamic port

--- app.py
+++ app.py
@@ -417,6 +417,6 @@ if __name__ == '__main__':
 
     app.run(
         host='0.0.0.0',   # Listen on all network interfaces
-        port=5000,
+        port=int(os.environ.get('PORT', 5000)),
         debug=False,       # Set True during development
     )
